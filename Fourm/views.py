df0e882f1aa69f2c98755e7998b3cbace31663ce
steven <steven.immune@gmail.com>
2023-07-04 07:19:33 +0200

    added html list

--- Fourm/views.py
+++ Fourm/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from .models import Question , Answer
 # Create your views here.
+
+
+def questions_list(request):
+    data = Question.objects.all()
+    return render(request,'Fourm/list.html',{'steven':data})
+
+
+
+
+
+
+
+def question_detail(request):
+    pass
